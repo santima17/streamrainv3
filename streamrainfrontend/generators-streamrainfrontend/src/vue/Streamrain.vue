3ab16253aa324b042946d749619929e410bc2b5e
Daniel Martinez <danielmartinezvigo@outlook.es>
2017-11-07 23:59:32 -0300

    algunos componentes para el frontend de generador

---
+++ streamrainfrontend/generators-streamrainfrontend/src/vue/Streamrain.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="streamrain">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand">Streamrain {{ config.tenant.name }}</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><router-link to="/">Home</router-link></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><router-link to="/login">Log In</router-link></li>
+            <li><router-link to="/signup"><span class="text-danger">Sign Up!</span></router-link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view 
+      :config="config"
+      :eventBus="eventBus"
+    >
+    </router-view>
+    <footer class="container-fluid text-center">
+      <p>Streamrain {{ config.tenant.name }} footer</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'streamrain',
+    props: [
+      'config',
+      'eventBus'
+    ],
+    data () {
+      return {
+      }
+    },
+    created () {
+    //   const updateJanusAlert = this.updateJanusAlert;
+    //   this.eventBus.$on('setJanusAlert', function (janusAlert) {
+    //     updateJanusAlert(janusAlert);
+    //   });
+    },
+    // methods: {
+    //   updateJanusAlert: function (janusAlert) {
+    //     this.janusAlert = janusAlert;
+    //   }
+    // }
+  }
+</script>
---
+++ streamrainfrontend/generators-streamrainfrontend/src/vue/session/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-fluid text-center">    
+    <div class="row content">
+      <div class="col-sm-2 sidenav">
+      </div>
+      <div class="col-sm-8 text-left">
+        <h1>Log In</h1>
+        <div class="row">
+          <div class="col-sm-4 text-left">
+            <div class="form-group">
+              <label class="control-label" for="focusedInput">Username</label>
+              <input v-model="username" class="form-control" type="text" value="Username">
+            </div>
+            <div class="form-group">
+              <label class="control-label">Password</label>
+              <input v-model="password" class="form-control" type="password" value="Password">
+            </div>
+            <div class="form-group">
+              <button class="btn btn-primary btn-lg">Log In</button>
+            </div>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="col-sm-2 sidenav">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        username: null,
+        password: null
+      }
+    },
+  }
+</script>
+
